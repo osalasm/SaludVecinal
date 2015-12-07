edca340418e4e772e9091991efe939b90e5740ea
osalasm <o.salas.mar@gmail.com>
2015-12-07 14:28:17 -0300

    Modificada vista home

--- application/views/home.php
+++ application/views/home.php
@@ -1,4 +1,11 @@
 <?php $this->load->view("cabecera")?>
-	<h1><?php if(isset($nombre)) echo $nombre ." ". $apellido?></h1>
+	<h1><?php if(isset($nombre)) echo "Bienvenido"." ".$nombre ." ". $apellido?></h1>
+	<table>
+			<ul>
+			<li><a href="#">Mi cuenta</a></li>
+			<li><a href="#">Reserva hora</a></li>
+			<li><a href="#">Salir</a></li>
+		</ul>
+	</table>
 </body>
 </html>
