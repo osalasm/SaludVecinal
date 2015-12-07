f35fd8e1176f37be7c6073e42fb5466ebae2920b
osalasm <o.salas.mar@gmail.com>
2015-12-07 14:30:58 -0300

    Modificado controlador home

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,12 +3,33 @@
 class Home extends CI_Controller {
 
 	public function index() {
-		
 		$this->load->model("usuario");
 		$this->usuario->encontrar(1);
 		$nombre = $this->usuario->getNombre();
 		$apellido = $this->usuario->getApellido();
-		$this->b = array("nombre" => $nombre, "apellido" => $apellido);
-		$this->load->view("home", $this->b);
+		if($this->_esVecino()) {
+			$this->vecino = array("nombre" => $nombre, "apellido" => $apellido);
+			$this->load->view("home", $this->vecino);
+		}
+		if($this->_esProfesional()) {
+			$this->profesional = array("nombre" => $nombre, "apellido" => $apellido);
+			$this->load->view("home2", $this->profesional);
+		}
+		if($this->_esAdministrador()) {
+			$this->administrador = array("nombre" => $nombre, "apellido" => $apellido);
+			$this->load->view("home3", $this->administrador);				
+		}
+	}
+
+	private function _esVecino() {
+		return false;
+	}
+
+	private function _esProfesional() {
+		return false;
+	}
+
+	private function _esAdministrador() {
+		return true;
 	}
 }
