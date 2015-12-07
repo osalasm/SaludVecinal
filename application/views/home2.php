cd20a8c3e698054c2f25d33862eb6f104dd9f9d2
osalasm <o.salas.mar@gmail.com>
2015-12-07 14:04:48 -0300

    Creada vista Profesional

---
+++ application/views/home2.php
@@ -0,0 +1,11 @@
+<?php $this->load->view("cabecera")?>
+	<h1><?php if(isset($nombre)) echo "Bienvenido"." ".$nombre ." ". $apellido?></h1>
+	<table>
+			<ul>
+			<li><a href="#">Mi cuenta</a></li>
+			<li><a href="#">Profesional</a></li>
+			<li><a href="#">Salir</a></li>
+		</ul>
+	</table>
+</body>
+</html>
