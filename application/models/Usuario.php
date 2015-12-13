ab939d5598c1f44ceaaa5873032bda7b859a3853
osalasm <o.salas.mar@gmail.com>
2015-12-13 19:03:56 -0300

    Update Usuario.php

--- application/models/Usuario.php
+++ application/models/Usuario.php
@@ -1,18 +1,39 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Usuario extends CI_Model {
+	private $id;
 	private $nombre;
 	private $apellido;
 	private $email;
+	private $rut;
 
-	public function encontrar($id) {
-		$sql = "SELECT * FROM usuario WHERE id = $id";
+	public function encontrarRut($rut) {
+		$sql = "SELECT * FROM usuario WHERE rut = $rut";
 		$query = $this->db->query($sql);
 		$row = $query->row();
+		
+		$this->id = $row->ID;
+		$this->nombre = $row->NOMBRE;
+		$this->apellido = $row->APELLIDO;
+		$this->email = $row->CORREO_ELECTRONICO;
+		$this->rut = $row->RUT;
+			
 
-		$this->nombre = $row->nombre;
-		$this->apellido = $row->apellido;
-		$this->email = $row->email;
+		#session_register("id");
+		#$_SESSION['id'] = $row->ID;
+	}
+
+	public function tipoUsuario(){
+		return $this->id;	
+	}
+
+	public function esCredencialValida($rut, $contrasena){
+		$query = $this->db->get_where('usuario', array('rut' => $rut, 'contrasena' => $contrasena));
+		return ($query->num_rows() > 0);
+	}
+
+	public function getRut() {
+		return $this->rut;
 	}
 
 	public function getNombre() {
@@ -22,7 +43,4 @@ class Usuario extends CI_Model {
 	public function getApellido() {
 		return $this->apellido;
 	}
-
-
-
 }
